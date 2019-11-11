648731810a5e2c6c091c0cf72eeff5a6aebaae63
autinerd <sidneyjohn23@kuyateh.eu>
2019-11-11 18:21:42 +0100

    add http

--- ardulumen.ino
+++ ardulumen.ino
@@ -12,16 +12,19 @@
 #elif defined(ESP8266)
 // ESP8266 specific
 	#include <ESP8266WiFi.h>
+	#include <ESP8266HTTPClient.h>
 #endif
-
-#include <WiFiClient.h>
 
 // LED-specific libarys
 #include <Adafruit_NeoPixel.h>
+#include <ArduinoJson.h>
 #include "src/pixelpp/PixelPP.h"
 #include "src/pixelpp/FillEffect.hpp"
 #include "src/pixelpp/SineEffect.hpp"
 #include "src/pixelpp/PixEffect.hpp"
+
+HTTPClient client;
+StaticJsonDocument<1024> json;
 
 // Initialize Objects
 // Preferences prefs;
@@ -34,6 +37,8 @@ PixelPP* animation = new PixelPP(65, strip.getPixels(), LEDColor::GRB);
 // Timing
 unsigned long last_frame = 0;
 const unsigned long frame_delay = 20;
+unsigned long last_poll = 0UL;
+const unsigned int polling_delay = 1000U;
 unsigned long now = 0;
 
 void setup()
@@ -42,17 +47,13 @@ void setup()
 	Serial.begin(115200);
 	Serial.print("ardulumen v");
 	Serial.println(VERSION);
-	// Initialize Preferences
-	// prefs.begin("ardulumen");
-	// Initialize WiFi AP
-	char apName[30] = "ardulumen";
-	// prefs.getString("apName","ardulumen").toCharArray(apName, 50);
-	Serial.print("Create AP with SSID "); Serial.println(apName);
 	animation->addEffect(new FillEffect(animation, (rgb){127, 0, 0}))
-        ->addEffect(new SineEffect(animation, 25, 2000))
-	      ->addEffect(new PixEffect(animation, (rgb){0, 0, 255}, 500, 1));
-  
-	//  WiFi.softAP(apName);
+			 ->addEffect(new SineEffect(animation, 25, 2000))
+			 ->addEffect(new PixEffect(animation, (rgb){0, 0, 255}, 500, 1));
+
+	// setup Wifi
+	WiFi.begin("ardulumen", emptyString);
+
 	strip.begin();
 	strip.setPixelColor(0,0,255,0);
 	strip.show();
@@ -67,5 +68,21 @@ void loop()
 		last_frame = now;
 		animation->render();
 		strip.show();
+	}
+	if ((now - last_poll) >= polling_delay)
+	{
+		last_poll = now;
+		if (client.begin("http://" + WiFi.gatewayIP().toString() + "/led"))
+		{
+			int httpCode = client.GET();
+			if (httpCode == HTTP_CODE_OK)
+			{
+				deserializeJson(json, client.getString());
+				Serial.println(json["instance"].as<uint8_t>());
+
+				// {"instance":0,"serial":0,"effects":[{"type":"fill","color":16711680},{"type":"sine","w":25,"p":2000},{"type":"pix","color":16711680,"f":200,"c":1}]}
+			}
+			client.end();
+		}
 	}
 }
