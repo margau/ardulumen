eb008e3ff4f66069eb93480f1035698bf36838ad
autinerd <sidneyjohn23@kuyateh.eu>
2019-11-12 11:58:05 +0100

    add json parsing

--- ardulumen.ino
+++ ardulumen.ino
@@ -3,6 +3,8 @@
  * Created by autinerd and margau
  */
 
+#define IfEffect(effect,class,...) if (effectType == effect) {animation->addEffect(new class(animation, __VA_ARGS__));}
+
 // Libarys for basic functions
 // ESP32 specific
 #if defined(ESP32)
@@ -18,16 +20,15 @@
 // LED-specific libarys
 #include <Adafruit_NeoPixel.h>
 #include <ArduinoJson.h>
-#include "src/pixelpp/PixelPP.h"
+#include "src/pixelpp/PixelPP.hpp"
 #include "src/pixelpp/FillEffect.hpp"
 #include "src/pixelpp/SineEffect.hpp"
 #include "src/pixelpp/PixEffect.hpp"
 
-HTTPClient client;
-StaticJsonDocument<1024> json;
-
 // Initialize Objects
 // Preferences prefs;
+HTTPClient client;
+StaticJsonDocument<1024> json;
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(150, 2, NEO_GRB + NEO_KHZ800);
 PixelPP* animation = new PixelPP(65, strip.getPixels(), LEDColor::GRB);
 
@@ -40,6 +41,8 @@ const unsigned long frame_delay = 20;
 unsigned long last_poll = 0UL;
 const unsigned int polling_delay = 1000U;
 unsigned long now = 0;
+int16_t currentSequence = -1;
+int16_t currentInstance = -1;
 
 void setup()
 {
@@ -47,9 +50,9 @@ void setup()
 	Serial.begin(115200);
 	Serial.print("ardulumen v");
 	Serial.println(VERSION);
-	animation->addEffect(new FillEffect(animation, (rgb){127, 0, 0}))
+	animation->addEffect(new FillEffect(animation, {127, 0, 0}))
 			 ->addEffect(new SineEffect(animation, 25, 2000))
-			 ->addEffect(new PixEffect(animation, (rgb){0, 0, 255}, 500, 1));
+			 ->addEffect(new PixEffect(animation, {0, 0, 255}, 500, 1));
 
 	// setup Wifi
 	WiFi.begin("ardulumen", emptyString);
@@ -72,17 +75,60 @@ void loop()
 	if ((now - last_poll) >= polling_delay)
 	{
 		last_poll = now;
-		if (client.begin("http://" + WiFi.gatewayIP().toString() + "/led"))
+		if (client.begin("http://" + WiFi.gatewayIP().toString() + (currentInstance == -1) ? "/led" : ("/led?instance=" + currentInstance)))
 		{
 			int httpCode = client.GET();
 			if (httpCode == HTTP_CODE_OK)
 			{
 				deserializeJson(json, client.getString());
-				Serial.println(json["instance"].as<uint8_t>());
-
-				// {"instance":0,"serial":0,"effects":[{"type":"fill","color":16711680},{"type":"sine","w":25,"p":2000},{"type":"pix","color":16711680,"f":200,"c":1}]}
+				analyzeRecievedJson();
 			}
 			client.end();
 		}
+	}
+}
+/**
+ * {
+ *	"instance": 0,
+ *	"serial": 0,
+ *	"effects":
+ *		[
+ *			{
+ *				"type": "fill",
+ *				"color": 16711680
+ *			},
+ *			{
+ *				"type": "sine",
+ *				"w": 25,
+ *				"p": 2000
+ *			},
+ *			{
+ *				"type": "pix",
+ *				"color": 16711680,
+ *				"f": 200,
+ *				"c": 1
+ *			}
+ *		]
+ * }
+ */
+void analyzeRecievedJson()
+{
+	if (currentInstance == -1)
+	{
+		currentInstance = json["instance"].as<int16_t>();
+	}
+	if (json["serial"].as<int16_t>() <= currentSequence || json["instance"].as<int16_t>() != currentInstance)
+	{
+		return;
+	}
+	animation->clearEffects();
+	JsonArray effects = json["effects"];
+	for (int8_t i = 0; i < effects.size(); i++)
+	{
+		JsonObject effect = effects[i];
+		String effectType = effect["type"].as<String>();
+		IfEffect("fill", FillEffect, ColorToRGB(effect["color"].as<uint32_t>()))
+		else IfEffect("sine", SineEffect, effect["w"].as<uint8_t>(), effect["p"].as<uint16_t>())
+		else IfEffect("pix", PixEffect, ColorToRGB(effect["color"].as<uint32_t>()), effect["f"].as<uint16_t>(), effect["c"].as<uint8_t>())
 	}
 }
